650d2933b6fb84263cbeb5829f22ad6a5c12e338
Leul <leulassefa255@gmail.com>
2023-09-07 04:28:59 -0700

    Python  import & modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,20 +1,45 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
+    from calculator_1 import *
+    n = len(argv)
 
-    if len(argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    if n != 4:
+        print("Usage: {} <a> <operator> <b>".format(argv[0]))
         exit(1)
 
-    num1 = int(argv[1])
-    num2 = int(argv[3])
-    options = ["+", "-", "*", "/"]
-    from calculator_1 import add, sub, mul, div
-    functions = [add, sub, mul, div]
-    for i, n in enumerate(options):
-        if argv[2] == n:
-            print("{} {} {} = {}".format(num1, n, num2, functions[i](num1, num2)))
-            break
-    else:
+    number1 = int(argv[1])
+    number2 = int(argv[3])
+    optn = argv[2]
+
+    def unknown_operator():
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
+
+    def add_function():
+        answer = add(number1, number2)
+        print("{:d} + {:d} = {:d}".format(number1, number2, answer))
+        return answer
+
+    def sub_function():
+        answer = sub(number1, number2)
+        print("{:d} - {:d} = {:d}".format(number1, number2, answer))
+        return answer
+
+    def mul_function():
+        answer = mul(number1, number2)
+        print("{:d} * {:d} = {:d}".format(number1, number2, answer))
+        return answer
+
+    def div_function():
+        answer = div(number1, number2)
+        print("{:d} / {:d} = {:d}".format(number1, number2, answer))
+        return answer
+
+    options = {
+        "+": add_function,
+        "-": sub_function,
+        "*": mul_function,
+        "/": div_function
+    }
+    options.get(optn, unknown_operator)()
