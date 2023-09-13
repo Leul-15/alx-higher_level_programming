1d4a3cc8596ea20e96ddc4508e17357a1e3ad03f
Leul <leulassefa255@gmail.com>
2023-09-13 10:46:29 -0700

    Python  More Data Structures Set Dictionary

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if not roman_string or not isinstance(roman_string, str):
         return 0
-    data = {'I': 1,'V': 5,'X': 10,'L': 50,'C': 100,'D': 500,'M': 1000}
+    data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
     prev = 0
     for i in range(len(roman_string) - 1, -1, -1):
