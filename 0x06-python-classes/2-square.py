07396b08cd9d4e7cb9510d6cf3fa03d1355d38b1
Leul <leulassefa255@gmail.com>
2023-09-26 11:26:07 -0700

    Python  Classes and Objects

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+"""
+This module defines a Square class
+"""
 class Square:
 
     def __init__(self, size=0):
+        """Initialize a new data"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
