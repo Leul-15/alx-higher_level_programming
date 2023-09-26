0de172f4d234515329f17303d111fb9cdc935dc4
Leul <leulassefa255@gmail.com>
2023-09-26 11:44:23 -0700

    Python  Classes and Objects

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-class Square:
-    """This module defines a Square class"""
+"""
+This module defines a Square class
+Its implements value and type checks for its attributes
+"""
 
+class Square:
+    """Square implementation
+    """
     def __init__(self, size=0):
-        """Square implementation"""
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
+        if type(size) != int:
+            raise TypeError('size must be an integer')
         elif size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
         self.__size = size
