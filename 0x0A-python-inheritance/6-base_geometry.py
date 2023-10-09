2adb2edba2d320fd6b5d72f62fbf0064bea40ec8
Leul <leulassefa255@gmail.com>
2023-10-09 05:20:53 -0700

    Python  Inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """ implementation"""
+    def area(self):
+        """ public instance method not implemented"""
+        raise Exception("area() is not implemented")
