587496b83b84fee588130cf0a9c03a763ae5d11f
Leul <leulassefa255@gmail.com>
2023-10-09 10:49:52 -0700

    Python  Inheritance

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-
 """ square class """
 Rectangle = __import__('9-rectangle').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Square(Rectangle):
-    """Represent a square."""
-
+    """ square """
     def __init__(self, size):
-        """Initialize a new square
-        """
-        self.integer_validator("size", size)
+        """ Initialize a new square """
         super().__init__(size, size)
+        self.integer_validator("size", size)
         self.__size = size
+
+    def __str__(self):
+        """ str """
+        string = "[Square] " + str(self.__size) + "/" + str(self.__size)
+        return string
