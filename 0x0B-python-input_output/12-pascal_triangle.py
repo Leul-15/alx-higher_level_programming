e6a3089590456c96c97e646d061d4946f8565fe9
Leul <leulassefa255@gmail.com>
2023-10-10 03:30:37 -0700

    Python  Input Output

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -4,13 +4,13 @@
 
 def pascal_triangle(n=4500):
     """print pascal"""
-    pascal = [[0]*i for i in range(1, n+1)]
+    p = [[0]*i for i in range(1, n+1)]
     cmpt = 0
     for i in range(n):
-        pascal[i][0] = 1
-        pascal[i][-1] = 1
+        p[i][0] = 1
+        p[i][-1] = 1
         for j in range(0, i//2):
-            pascal[i][j+1] = pascal[i-1][j] + pascal[i-1][j+1]
-            pascal[i][i-j-1] = pascal[i-1][j] + pascal[i-1][j+1]
+            p[i][j+1] = p[i-1][j] + p[i-1][j+1]
+            p[i][i-j-1] = p[i-1][j] + p[i-1][j+1]
 
-    return pascal
+    return p
