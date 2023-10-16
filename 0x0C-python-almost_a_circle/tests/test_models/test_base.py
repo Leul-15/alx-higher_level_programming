ef7788c7cc43e45d52f22b2c159295e31a05fed6
Leul <leulassefa255@gmail.com>
2023-10-16 12:22:27 -0700

    Python  Almost a circle

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,32 +1,103 @@
 #!/usr/bin/python3
-""" Tests for class Base """
+
+"""Defines unittests for base.py.
+
+Unittest classes:
+    TestBase_instantiation
+    TestBase_to_json_string
+    TestBase_save_to_file
+    TestBase_from_json_string
+    TestBase_create
+    TestBase_load_from_file
+"""
+import os
 import unittest
-from models.base import Base
-from models.rectangle import Rectangle
-from models.square import Square
-import pep8
+from base import Base
+from rectangle import Rectangle
+from square import Square
 
 
-class TestBase(unittest.TestCase):
-    """ tests for class base """
+class TestBase_instantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the Base class."""
 
-    def test_id(self):
+    def test_no_arg(self):
         base1 = Base()
-        self.assertEqual(base1.id, 1)
         base2 = Base()
-        self.assertEqual(base2.id, 2)
-        base3 = Base()
-        self.assertEqual(base3.id, 3)
-        base4 = Base(12)
-        self.assertEqual(base4.id, 12)
-        base5 = Base()
-        self.assertEqual(base5.id, 4)
-        with self.assertRaises(TypeError):
-            Base(5, 6)
+        self.assertEqual(base1.id, base2.id - 1)
 
-    def test_pep8(self):
-        """ tests prp8 formating """
-        pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['models/base.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style erros (and warnings).")
+
+class TestBase_to_json_string(unittest.TestCase):
+    """Unittests for testing to_json_string method of Base class."""
+
+    def test_to_json_string_empty_list(self):
+        self.assertEqual("[]", Base.to_json_string([]))
+
+    def test_to_json_string_none(self):
+        self.assertEqual("[]", Base.to_json_string(None))
+
+    def test_to_json_string_no_args(self):
+        with self.assertRaises(TypeError):
+            Base.to_json_string()
+
+    def test_to_json_string_more_than_one_arg(self):
+        with self.assertRaises(TypeError):
+            Base.to_json_string([], 1)
+
+
+class TestBase_save_to_file(unittest.TestCase):
+    """Unittests for testing save_to_file method of Base class."""
+
+    def test_save_to_file_one_rectangle(self):
+        r = Rectangle(10, 7, 2, 8, 5)
+        Rectangle.save_to_file([r])
+        with open("Rectangle.json", "r") as file:
+            self.assertTrue(len(file.read()) == 53)
+
+
+class TestBase_from_json_string(unittest.TestCase):
+    """Unittests for testing from_json_string method of Base class."""
+
+    def test_from_json_string_None(self):
+        self.assertEqual([], Base.from_json_string(None))
+
+    def test_from_json_string_empty_list(self):
+        self.assertEqual([], Base.from_json_string("[]"))
+
+    def test_from_json_string_no_args(self):
+        with self.assertRaises(TypeError):
+            Base.from_json_string()
+
+    def test_from_json_string_more_than_one_arg(self):
+        with self.assertRaises(TypeError):
+            Base.from_json_string([], 1)
+
+
+class TestBase_create(unittest.TestCase):
+    """Unittests for testing create method of Base class."""
+
+    def test_create_rectangle_original(self):
+        rectangle1 = Rectangle(3, 5, 1, 2, 7)
+        r1_dictionary = rectangle1.to_dictionary()
+        rectangle2 = Rectangle.create(**r1_dictionary)
+        self.assertEqual("[Rectangle] (7) 1/2 - 3/5", str(rectangle1))
+
+    def test_create_rectangle_new(self):
+        rectangle1 = Rectangle(3, 5, 1, 2, 7)
+        r1_dictionary = rectangle1.to_dictionary()
+        rectangle2 = Rectangle.create(**r1_dictionary)
+        self.assertEqual("[Rectangle] (7) 1/2 - 3/5", str(rectangle2))
+
+
+class TestBase_load_from_file(unittest.TestCase):
+    """Unittests for testing load_from_file_method of Base class."""
+
+    def test_load_from_file_first_rectangle(self):
+        rectangle1 = Rectangle(10, 7, 2, 8, 1)
+        rectangle2 = Rectangle(2, 4, 5, 6, 2)
+        Rectangle.save_to_file([rectangle1, rectangle2])
+        list_rectangles_output = Rectangle.load_from_file()
+        self.assertEqual(str(rectangle1), str(list_rectangles_output[0]))
+
+    def test_load_from_file_more_than_one_arg(self):
+        with self.assertRaises(TypeError):
+            Base.load_from_file([], 1)
