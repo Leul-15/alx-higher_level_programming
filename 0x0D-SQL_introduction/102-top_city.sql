3ec00d7e9a881d96451def1bc374f58503c042e6
Leul <leulassefa255@gmail.com>
2023-11-14 07:25:49 -0800

    SQL  Introduction

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,6 @@
+-- displays the top 3 of cities temperature
+-- temperatures between July and August
+SELECT `city`, AVG(`value`) AS `avg_temp` FROM `temperatures`
+WHERE `month` = 7 OR `month` = 8
+GROUP BY `city`
+ORDER BY `avg_temp` DESC LIMIT 3;
