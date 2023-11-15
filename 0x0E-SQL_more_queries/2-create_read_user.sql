308ed45144c9b74b44a7b7a414c8134f3890845d
Leul <leulassefa255@gmail.com>
2023-11-15 05:14:37 -0800

    SQL  More queries

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,5 +3,5 @@
 -- Script that creates the database hbtn_0d_2 and user user_0d_2
 -- Create or use the existing database hbtn_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
