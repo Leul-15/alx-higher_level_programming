cb1da65a9720caf6e93f07e56c4a64478cda69b7
Leul <leulassefa255@gmail.com>
2023-11-15 04:42:07 -0800

    SQL  More queries

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- lists all shows contained in the database hbtn_0d_tvshows
+-- If a show doesn’t have a genre, display NULL
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
