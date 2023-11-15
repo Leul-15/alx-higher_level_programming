257c7f56b297e93797bba69197d28d60b77b4237
Leul <leulassefa255@gmail.com>
2023-11-15 05:03:56 -0800

    SQL  More queries

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,13 @@
 -- creates the database hbtn_0d_2 and the user user_0d_2
 -- The user_0d_2 password should be set to user_0d_2_pwd
 -- Script that creates the database hbtn_0d_2 and user user_0d_2
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+CREATE DATABASE
+    IF NOT EXISTS `hbtn_0d_2`;
+CREATE USER
+    IF NOT EXISTS 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT
+   ON `hbtn_0d_2`.*
+   TO 'user_0d_2'@'localhost'
+   IDENTIFIED BY 'user_0d_2_pwd';
+FLUSH PRIVILEGES;
