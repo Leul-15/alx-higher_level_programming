cdead58cd5070851f0365ed04d827942b80426f4
Leul <leulassefa255@gmail.com>
2023-12-16 04:16:21 -0800

    Python  Object relational mapping

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Displays all values in the states table of the database hbtn_0e_0_usa
+whose name matches that supplied as argument
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM `states`")
+    [print(state) for state in cursor.fetchall() if state[1] == sys.argv[4]]
