4ffc5be654838c2ac814cd7dd6df9d21637fc563
Leul <leulassefa255@gmail.com>
2023-12-16 04:01:41 -0800

    Python  Object relational mapping

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Lists all states with a name starting with\
+    N from the database hbtn_0e_0_usa.
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(
+        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]
+    )
+    cursor = database.cursor()
+    cursor.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in cursor.fetchall() if state[1][0] == "N"]
