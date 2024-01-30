ba30bd9183cf55dfcc782a1ec2fa3f5c91506efc
Leul <leulassefa255@gmail.com>
2024-01-30 08:33:41 -0800

    JavaScript  Web scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,14 +1,15 @@
 #!/usr/bin/node
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
 request(url, function (error, response, body) {
   if (!error) {
-    let characters = JSON.parse(body).characters;
+    const characters = JSON.parse(body).characters;
     printCharacters(characters, 0);
   }
 });
 
-function printCharacters (characters, index) {
+function printCharacters(characters, index) {
   request(characters[index], function (error, response, body) {
     if (!error) {
       console.log(JSON.parse(body).name);
